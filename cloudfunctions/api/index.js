62454cf7dd44802fea793ac32c5c37be03463cc1
443831225@qq.com <443831225@qq.com>
2021-03-24 15:34:20 +0800

    第一次提交

---
+++ cloudfunctions/api/index.js
@@ -0,0 +1,40 @@
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: 'zwly-5gx61idr72dceef1',
+  traceUser: true
+})
+
+const router = require('./router/index.js')
+/*
+*
+* @reutrn {}
+* code  0、成功 1、失败 2、成功并且展示msg
+* msg   请求消息
+* data  返回数据
+*/
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  console.log(event)
+  const { url, data } = event
+  const path = url.split('/')
+  const result = await router[path[0]][path[1]](data).catch(err => {
+    console.log(err)
+    const msg = err.errMsg || '网络错误'
+    return {
+      code: 1,
+      msg,
+      data: null
+    }
+  })
+  if (!result.msg) {
+    result.msg = 'suceess!'
+  }
+  if (!result.code) {
+    result.code = 0
+  }
+  if (!result.data) {
+    result.data = null
+  }
+  return result
+}
